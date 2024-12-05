14fa22890702cfca52fc07dbc8bad804a33adee5
Wojciech Konieczkowicz <wojtek.konieczkowicz@gmail.com>
2024-12-05 17:24:08 +0100

    Update train shell script to the final form

--- train_xl.sh
+++ train_xl.sh
@@ -1,3 +1,6 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch train_xl.py \
     --gradient_checkpointing \
-    --use_8bit_adam
+    --use_8bit_adam \
+    --inference_every 5 \
+    --run_name 2024-12-06 \
+    --upload_to_s3
